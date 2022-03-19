29eb955f608464536fc77934c10c9ee62ff78766
Alejandro Medina <alejandro@radishlab.com>
2022-03-19 18:33:14 -0300

    Add main container in main App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Container } from "@mui/material";
 import NavBar from "./components/NavBar/NavBar";
+import { shadows } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <h2>Qué Buena Remera!</h2>
+      <Container className="MainContainer" maxWidth="lg" sx={{ boxShadow: 3 }}>
+
+      </Container>
     </div>
   );
 }
