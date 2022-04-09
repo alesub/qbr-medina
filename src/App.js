401c13238c2f97aab27648b3f19def99af1162a9
Alejandro Medina <alejandro@radishlab.com>
2022-04-09 16:02:01 -0300

    Add product detail route in main App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Container } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <Container className="MainContainer" maxWidth="lg" sx={{ boxShadow: 3 }}>
-        <ItemListContainer />
-      </Container>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<ItemListContainer />}/>
+          <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
